02dd0151be70168f4adfe3ef44e7bc02c2094e4f
Your Name <you@example.com>
2015-08-04 23:22:17 -0700

    new

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ var SampleApp = function() {
         self.routes['/game/players'] = function(req, res) {
             var temp;
             request("http://home.tjbenator.com:7878/v2/users/activelist?token=90747CF82B55B6F8E91C3D043CCB4A229453CD65AB87A722D8A1ECB057C8FF22", function(error, response, body){
-                console.log(body.activeusers);
+                console.log( JSON.parse(body).activeusers );
             });
 
             //res.send("Number of players online: " + num_players + "\n Playerlist: " + playerlist);
