dc00baabc93e7472253c3176c246393d19d30ae2
Your Name <you@example.com>
2015-08-04 23:05:07 -0700

    checking temp.activeusers

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ var SampleApp = function() {
         self.routes['/game/players'] = function(req, res) {
             var temp;
             request("http://home.tjbenator.com:7878/v2/users/activelist?token=90747CF82B55B6F8E91C3D043CCB4A229453CD65AB87A722D8A1ECB057C8FF22", function(error, response, body){
-                temp = body;
+                temp = JSON.parse(body);
             });
 
             res.setHeader('Content-Type', 'text/plain');
